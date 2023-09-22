50bee2a3be95eca6a327232d970526725ede0fc3
toto <antoniolabate19@gmail.com>
2023-09-22 23:32:01 +0200

    added pptx support

--- to_plain_text.py
+++ to_plain_text.py
@@ -1,6 +1,7 @@
 import sys
 import pypandoc
 import PyPDF2
+from pptx import Presentation
 
 def todo():
     return print_return_error("Not yet implemented.")
@@ -23,6 +24,20 @@ def from_docx(filepath):
 
 def from_doc(filepath):
     return with_pandoc(filepath)
+
+def from_pptx(filepath):
+    presentation = Presentation(filepath)
+    extracted_text = []
+    for slide in presentation.slides:
+        slide_text = ""
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                for paragraph in shape.text_frame.paragraphs:
+                    for run in paragraph.runs:
+                        slide_text += run.text + "\n"
+        extracted_text.append(slide_text)
+    return "\n".join(extracted_text)
+
 
 def from_pdf(filepath):
     try:
@@ -62,6 +77,8 @@ def get_content(filepath):
         return from_docx(filepath)
     elif ext == "doc":
         return from_doc(filepath)
+    elif ext == "pptx":
+        return from_pptx(filepath)
     else:
         return print_return_error(f"Extension {ext} is not yet supported.")
         
